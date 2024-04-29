bc249ee39a236a9315fd2f21887aa7bde399816a
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:55:12 +0300

    Update LCD_Interface.h

--- 2-HAL/LCD/LCD_Interface.h
+++ 2-HAL/LCD/LCD_Interface.h
@@ -10,15 +10,60 @@
 
 
 
-#define LCD_u8_LINE0			0
-#define LCD_u8_LINE1			1
-
-#define LCD_SHIFT_RIGHT			0
-#define LCD_SHIFT_LEFT			1
 
 
 
+//-------------------------< Select Copy_u8LineNum>-----------------------------------
 
+#define LCD_u8_LINE0				0
+#define LCD_u8_LINE1				1
+
+//-------------------------< Select Copy_u8Locat >------------------------------------
+
+#define LCD_u8_COLUMN0				0
+#define LCD_u8_COLUMN1				1
+#define LCD_u8_COLUMN2				2
+#define LCD_u8_COLUMN3				3
+#define LCD_u8_COLUMN4				4
+#define LCD_u8_COLUMN5				5
+#define LCD_u8_COLUMN6				6
+#define LCD_u8_COLUMN7				7
+#define LCD_u8_COLUMN8				8
+#define LCD_u8_COLUMN9				9
+#define LCD_u8_COLUMN10				10
+#define LCD_u8_COLUMN11				11
+#define LCD_u8_COLUMN12				12
+#define LCD_u8_COLUMN13				13
+#define LCD_u8_COLUMN14				14
+#define LCD_u8_COLUMN15				15
+
+//------------------------------< Select Copy_u8CGCharNum >----------------------------------------------
+
+#define LCD_u8_CG_CHAR1			0
+#define LCD_u8_CG_CHAR2			1
+#define LCD_u8_CG_CHAR3			2
+#define LCD_u8_CG_CHAR4			3
+#define LCD_u8_CG_CHAR5			4
+#define LCD_u8_CG_CHAR6			5
+#define LCD_u8_CG_CHAR7			6
+#define LCD_u8_CG_CHAR8			7
+
+//-----------------------------< Config Init Mode & Shift Direction >-----------------------------------
+
+#define LCD_SHIFT_RIGHT			1
+#define LCD_SHIFT_LEFT			2
+
+
+#define LCD_4BIT_MODE			1		//DB4 = 0 ---> Data sent or recieved through (DB4 ---> DB7)
+#define LCD_8BIT_MODE			2		//DB4 = 1 ---> Data sent or recieved through (DB0 ---> DB7)
+
+
+
+
+
+
+
+//------------------------------< Functions Prototypes >----------------------------------
 
 void LCD_voidInit(void);
 
@@ -26,17 +71,7 @@ void LCD_voidSendCmnd(u8 Copy_u8cmnd);
 
 void LCD_voidSendChar(u8 Copy_u8Char);
 
-/* 
-   (Copy_u8CGLocat)options:  - LCD_u8_CGRAM_CHAR1
-							 - LCD_u8_CGRAM_CHAR2
-							 - LCD_u8_CGRAM_CHAR3	
-							 - LCD_u8_CGRAM_CHAR4 
-							 - LCD_u8_CGRAM_CHAR5 
-							 - LCD_u8_CGRAM_CHAR6 
-							 - LCD_u8_CGRAM_CHAR7 
-							 - LCD_u8_CGRAM_CHAR8  */ 
-
-void LCD_voidSendSpecialChar(u8 Copy_u8CGLocat, u8 Copy_u8LineNum, u8 Copy_u8Locat, u8* Copy_pu8Pattern)
+void LCD_voidSendSpecialChar(u8 Copy_u8CGCharNum, u8 Copy_u8LineNum, u8 Copy_u8Locat, u8* Copy_pu8Pattern);
 
 void LCD_voidSendNumber(u32 Copy_u32Number);
 
