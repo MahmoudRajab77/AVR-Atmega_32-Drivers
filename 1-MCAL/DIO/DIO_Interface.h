49fb17781b79d4843b1b529a4aba930d28cf2f71
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:03:22 +0300

    Update DIO_Interface.h

--- 1-MCAL/DIO/DIO_Interface.h
+++ 1-MCAL/DIO/DIO_Interface.h
@@ -7,13 +7,13 @@
 #ifndef DIO_INTERFACE_H
 #define DIO_INTERFACE_H
 
-/*Macros for Ports ID */
+/************* Macros for Ports ID ****************/
 #define DIO_u8_PORTA        0
 #define DIO_u8_PORTB        1
 #define DIO_u8_PORTC        2
 #define DIO_u8_PORTD        3
 
-/* Macros for PINS ID */
+/************* Macros for PINS ID *****************/
 #define DIO_u8_PIN0			0
 #define DIO_u8_PIN1			1
 #define DIO_u8_PIN2			2
@@ -23,17 +23,22 @@
 #define DIO_u8_PIN6			6
 #define DIO_u8_PIN7			7
 
-/* Macros for PIN Direction */
+/************* Macros for PIN Direction ***********/
 #define DIO_u8_INPUT		0
 #define DIO_u8_OUTPUT		1
 
-/* Macros for pin value */
+#define DIO_PORT_OUTPUT		0xFF
+#define DIO_PORT_INPUT		0x00
+
+/************* Macros for pin value ***************/
 #define DIO_u8_HIGH			1
 #define DIO_u8_LOW			0
 
-/* prototypes of the DIO Driver */
+
+/***************** prototypes of the DIO Functions *********************/
 
 void DIO_voidInit(void);
+
 u8 DIO_u8SetPinDirection(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinDirection);
 u8 DIO_u8SetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinValue);
 u8 DIO_u8GetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId, u8* Copy_pu8ReturnedPinValue);
