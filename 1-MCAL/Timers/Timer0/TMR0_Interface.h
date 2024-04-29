fbcc80e4870516994f2bbea4cf2b01c402e99048
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:30:35 +0300

    Update TMR0_Interface.h

--- 1-MCAL/Timers/Timer0/TMR0_Interface.h
+++ 1-MCAL/Timers/Timer0/TMR0_Interface.h
@@ -12,10 +12,10 @@
 
 //---------------< Macros for Timer0 Modes >-------------------------
 
-#define TMR0_NORMAL_MODE					0
-#define TMR0_CTC_MODE						1
+#define TMR0_NORMAL_MODE					    0
+#define TMR0_CTC_MODE						      1
 #define TMR0_PHASE_CORRECT_MODE				2	
-#define TMR0_FAST_PWM_MODE					3
+#define TMR0_FAST_PWM_MODE					  3
 
 //----------------< Macros for Non-PWM Output Modes >-----------------
 
@@ -26,23 +26,23 @@
 
 //---------------< Macros for Fast-PWM Output Modes >-----------------
 
-#define TMR0_NORMAL_OC_DISCNCTD				0
+#define TMR0_NORMAL_OC_DISCNCTD				  0
 #define TMR0_FAST_PWM_OC_NON_INVRTD			1
-#define TMR0_FAST_PWM_OC_INVRTD				2
+#define TMR0_FAST_PWM_OC_INVRTD				  2
 
 //---------------< Macros for Phase-Correct Output Modes >-------------
 
-#define TMR0_NORMAL_OC_DISCNCTD				0
+#define TMR0_NORMAL_OC_DISCNCTD				  0
 #define TMR0_PhCORCT_OC_NON_INVRTD			1
-#define TMR0_PhCORCT_OC_INVRTD				2
+#define TMR0_PhCORCT_OC_INVRTD				  2
 
 //--------------------< Macros for Clk Modes >--------------------------
 
-#define TMR0_CLK_NO_PRSCLR					0
-#define TMR0_CLK_PRSCLR_8					1
-#define TMR0_CLK_PRSCLR_64					2
-#define TMR0_CLK_PRSCLR_256					3
-#define TMR0_CLK_PRSCLR_1024				4
+#define TMR0_CLK_NO_PRSCLR					  0
+#define TMR0_CLK_PRSCLR_8				  	  1
+#define TMR0_CLK_PRSCLR_64					  2
+#define TMR0_CLK_PRSCLR_256					  3
+#define TMR0_CLK_PRSCLR_1024				  4
 #define TMR0_EXT_CLK_T0_Fall_EDGE			5
 #define TMR0_EXT_CLK_T0_RIS_EDGE			6
 
