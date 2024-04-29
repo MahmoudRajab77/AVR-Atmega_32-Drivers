25f51ece4821ef48d34f53315ad0972265f7de0d
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:28:57 +0300

    Update TMR0_Config.h

--- 1-MCAL/Timers/Timer0/TMR0_Config.h
+++ 1-MCAL/Timers/Timer0/TMR0_Config.h
@@ -15,9 +15,9 @@
 //--------------< Timer0 Modes >--------------
 
 /* options : - TMR0_NORMAL_MODE
-		     - TMR0_CTC_MODE
-		     - TMR0_PHASE_CORRECT_MODE	
-		     - TMR0_FAST_PWM_MODE		
+	     - TMR0_CTC_MODE
+	     - TMR0_PHASE_CORRECT_MODE	
+	     - TMR0_FAST_PWM_MODE		
 */
 
 
@@ -34,23 +34,23 @@
 //--------------< Output Mode for Non-PWM >--------------
 
 /* options : - TMR0_NORMAL_OC_DISCNCTD
-			 - TMR0_TOGGLE_OC_COMP_MATCH
-			 - TMR0_SET_OC_COMP_MATCH
-			 - TMR0_CLEAR_OC_COMP_MATCH
+	     - TMR0_TOGGLE_OC_COMP_MATCH
+	     - TMR0_SET_OC_COMP_MATCH
+	     - TMR0_CLEAR_OC_COMP_MATCH
 */			 
 
 //--------------< Output modes for Fast PWM >--------------
 
 /* options : - TMR0_NORMAL_OC_DISCNCTD
-			 - TMR0_FAST_PWM_OC_NON_INVRTD
-			 - TMR0_FAST_PWM_OC_INVRTD	
+	     - TMR0_FAST_PWM_OC_NON_INVRTD
+	     - TMR0_FAST_PWM_OC_INVRTD	
 */
 
 //--------------< Output modes for Phase Correct >--------------
 
 /* options : - TMR0_NORMAL_OC_DISCNCTD
-			 - TMR0_PhCORCT_OC_NON_INVRTD
-			 - TMR0_PhCORCT_OC_INVRTD
+	     - TMR0_PhCORCT_OC_NON_INVRTD
+	     - TMR0_PhCORCT_OC_INVRTD
 */			 
 
 
@@ -58,7 +58,7 @@
 
 //**********************< Choose the Output Mode for your Timer >**********************
 
-#define TMR0_u8_OUTPUT_MODE			TMR0_NORMAL_OC_DISCONNECTED
+#define TMR0_u8_OUTPUT_MODE			TMR0_NORMAL_OC_DISCNCTD
 
 
 
