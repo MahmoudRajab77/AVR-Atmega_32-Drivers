3125e152fb0d60b703deea7e67ba4eb21d2cd0f2
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:57:15 +0300

    Update LCD_Program.c

--- 2-HAL/LCD/LCD_Program.c
+++ 2-HAL/LCD/LCD_Program.c
@@ -7,13 +7,13 @@
 /* LCD_program.c*/
 
 /* LIB */
-#include "STD_TYPES.h"
-#include "BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
 #define F_CPU 	8000000UL
 #include <util/delay.h>
 
 /* MCAL */
-#include "DIO_interface.h"
+#include "../../MCAL/DIO/DIO_interface.h"
 
 /* HAL */
 #include "LCD_Interface.h"
@@ -23,54 +23,57 @@
 
 void LCD_voidInit(void)
 {
-	switch (LCD_u8_INIT_MODE)
-	{
-		case LCD_4BIT_MODE:
-		
-			_delay_ms(35);
-			
-			LCD_voidSendCmnd(0b00100000);
-			LCD_voidSendCmnd(0b00100000);
-			LCD_voidSendCmnd(0b11000000);
-			
-			_delay_us(40);
-			
-			LCD_voidSendCmnd(0b00000000);
-			LCD_voidSendCmnd(0b11110000);
-			
-			_delay_us(40);
-			
-			LCD_voidSendCmnd(0b00000000);
-			LCD_voidSendCmnd(0b00010000);
-			
-			_delay_ms(2);
-			
-			LCD_voidSendCmnd(0b00000000);
-			LCD_voidSendCmnd(0b01100000);
-			
-		break;
-		
-		case LCD_8BIT_MODE:
-		
-			_delay_ms(35);
-			/* function set cmnd  0b00111000 */
-			LCD_voidSendCmnd(0b00111000);
-			_delay_us(40);
-			/* Display on/off control */
-			LCD_voidSendCmnd(0b00001111);
-			_delay_us(40);
-			/* Display clear */
-			LCD_voidSendCmnd(0b00000001);
-			_delay_ms(2);
-			/* Entry mode set */
-			LCD_voidSendCmnd(0b00000110);
-		
-		break;
-		
-		default:
-		break;
-	}// end switch 
+	DIO_u8SetPinDirection(LCD_u8_CONTROL_PORT,LCD_u8_E_PIN,DIO_u8_OUTPUT);
+	DIO_u8SetPinDirection(LCD_u8_CONTROL_PORT,LCD_u8_Rw_PIN,DIO_u8_OUTPUT);
+	DIO_u8SetPinDirection(LCD_u8_CONTROL_PORT,LCD_u8_Rs_PIN,DIO_u8_OUTPUT);
 	
+#if LCD_u8_INIT_MODE == LCD_4BIT_MODE
+		
+	DIO_u8SetPortDirection(LCD_u8_DATA_PORT, 0b1111);
+	
+	
+	_delay_ms(35);
+			
+	LCD_voidSendCmnd(0b00100000);
+	LCD_voidSendCmnd(0b00100000);
+	LCD_voidSendCmnd(0b11000000);
+			
+	_delay_us(40);
+			
+	LCD_voidSendCmnd(0b00000000);
+	LCD_voidSendCmnd(0b11000000);	// Cursor OFF & Blinking OFF
+			
+	_delay_us(40);
+			
+	LCD_voidSendCmnd(0b00000000);
+	LCD_voidSendCmnd(0b00010000);
+			
+	_delay_ms(2);
+			
+	LCD_voidSendCmnd(0b00000000);
+	LCD_voidSendCmnd(0b01100000);
+			
+		
+#elif LCD_u8_INIT_MODE == LCD_8BIT_MODE
+		
+	DIO_u8SetPortDirection(LCD_u8_DATA_PORT, DIO_PORT_OUTPUT);
+	
+	_delay_ms(35);
+	/* function set cmnd  0b00111000 */
+	LCD_voidSendCmnd(0b00111000);
+	_delay_us(40);
+	/* Display on/off control */
+	LCD_voidSendCmnd(0b00001100);
+	_delay_us(40);
+	/* Display clear */
+	LCD_voidSendCmnd(0b00000001);
+	_delay_ms(2);
+	/* Entry mode set */
+	LCD_voidSendCmnd(0b00000110);
+	
+#else
+	#error "Wrong LCD_MODE Configuration parameter"
+#endif	
 }// end Func
 
 //-----------------------------------------------------------------------------------------------
@@ -78,68 +81,53 @@ void LCD_voidInit(void)
 void LCD_voidSendCmnd(u8 Copy_u8cmnd)
 {
 	
-	switch (LCD_u8_INIT_MODE)
-	{
-		case LCD_8BIT_MODE:
-			
-			/* Rs = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_LOW);
+	/* Rs = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_LOW);
 		
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
+	/* Rw = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
 		
-			/* send cmnd Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, Copy_u8cmnd); // send the entire byte 
+#if LCD_u8_INIT_MODE == LCD_8BIT_MODE
+
+	/* send cmnd Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, Copy_u8cmnd); // send the entire byte 
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+
+	
+	//--------------------------------------------------------------------
+#elif LCD_u8_INIT_MODE == LCD_4BIT_MODE
 		
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-			
-		break;
-		//--------------------------------------------------------------------
-		case LCD_4BIT_MODE:
-		
-			//---------------------< OPERATION 1 >----------------------------
-			
-			/* Rs = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_LOW);
-			
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
-			
-			/* send cmnd Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, (Copy_u8cmnd & 0xf0)); // send the last 4 bits first
-			
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-			
-			//---------------------< OPERATION 2 >----------------------------
-			
-			/* Rs = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_LOW);
-			
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
-			
-			/* send cmnd Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, ((Copy_u8cmnd << 4) & 0xf0)); // send the first 4 bits of the byte
-			
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-		
-		break;
-		
-		default:
-		break;
-	}
+	//---------------------< OPERATION 1 >----------------------------
+
+	/* send cmnd Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, (Copy_u8cmnd & 0xf0)); // send the last 4 bits first
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+
+	//---------------------< OPERATION 2 >----------------------------
+	
+	/* send cmnd Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, ((Copy_u8cmnd << 4) & 0xf0)); // send the first 4 bits of the byte
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+	
+
+#else
+	#error "Wrong LCD_MODE Configuration parameter"
+#endif		
 	
 }//end send cmnd function
 
@@ -147,70 +135,54 @@ void LCD_voidSendCmnd(u8 Copy_u8cmnd)
 
 void LCD_voidSendChar(u8 Copy_u8Char)
 {
-	switch (LCD_u8_INIT_MODE)
-	{
-		case LCD_8BIT_MODE:
-		
-			/* Rs = 1 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_HIGH);
-			
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
-			
-			/* send char Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, Copy_u8char);
-			
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-		
-		break;
-		
-		case LCD_4BIT_MODE:
-			
-			//----------------------< OPERATION 1 >----------------------------
-			
-			/* Rs = 1 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_HIGH);
-			
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
-			
-			/* send char Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, (Copy_u8char & 0xf0));		// send the last 4 bit of the byte first
-			
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-		
-			//----------------------< OPERATION 2 >----------------------------
-			
-			/* Rs = 1 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_HIGH);
-			
-			/* Rw = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
-			
-			/* send char Byte to the data pins of LCD */
-			DIO_u8SetPortValue(LCD_u8_DATA_PORT, ((Copy_u8char << 4) & 0xf0));		// send the first 4 bit of the byte 
-			
-			/* Pulse of enable pin E = 1 , E = 0 */
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
-			_delay_us(1);
-			DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
-			_delay_us(1);
-			
-		break;
-		
-		default:
-		break;
-		
-	}// end switch 
+
+	/* Rs = 1 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rs_PIN, DIO_u8_HIGH);
 	
+	/* Rw = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_Rw_PIN, DIO_u8_LOW);
+
+#if LCD_u8_INIT_MODE ==	LCD_8BIT_MODE
+
+	/* send char Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, Copy_u8Char);
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+		
+
+		
+#elif LCD_u8_INIT_MODE ==	LCD_4BIT_MODE
+
+	//----------------------< OPERATION 1 >----------------------------
+	
+	/* send char Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, (Copy_u8Char & 0xf0));		// send the last 4 bit of the byte first
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+		
+	//----------------------< OPERATION 2 >----------------------------
+	
+	/* send char Byte to the data pins of LCD */
+	DIO_u8SetPortValue(LCD_u8_DATA_PORT, ((Copy_u8Char << 4) & 0xf0));		// send the first 4 bit of the byte
+
+	/* Pulse of enable pin E = 1 , E = 0 */
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_HIGH);
+	_delay_us(1);
+	DIO_u8SetPinValue(LCD_u8_CONTROL_PORT, LCD_u8_E_PIN, DIO_u8_LOW);
+	_delay_us(1);
+	
+#else 
+	#error	"Wrong LCD_MODE Configuration parameter"
+#endif
+
 }//end send char function 
 
 //---------------------------------------------------------------------------------------------------------
@@ -254,9 +226,9 @@ u8 LCD_u8SendStr(u8* Copy_pu8Str)
 	
 	if (Copy_pu8Str != NULL)
 	{
-		for (Local_u8Index = 0 ; *(Copy_pu8Str + Local_u8Index) != '\0' ; ++Local_u8Index)
+		for (Local_u8Index = 0 ; Copy_pu8Str[Local_u8Index] != '\0' ; Local_u8Index++)
 		{
-			LCD_u8SendChar(*(Copy_pu8Str + Local_u8Index));
+			LCD_voidSendChar(Copy_pu8Str[Local_u8Index]);
 		}	
 	}
 	else 
@@ -278,26 +250,22 @@ void LCD_voidClear(void)
 
 //----------------------------------------------------------------------------------------------------------
 
-void LCD_voidSendSpecialChar(u8 Copy_u8CGLocat, u8 Copy_u8LineNum, u8 Copy_u8Locat, u8* Copy_pu8Pattern)
+void LCD_voidSendSpecialChar(u8 Copy_u8CGCharNum, u8 Copy_u8LineNum, u8 Copy_u8Locat, u8* Copy_pu8Pattern)
 {
+	// Variables
+	u8 Local_u8CGRAMAddress = 0;
+
+
+	// Calculating the CGRAM Address
+	Local_u8CGRAMAddress = Copy_u8CGCharNum * 8;
+
+	// Set the 6th bit of the Address to HIGH
+	SET_BIT(Local_u8CGRAMAddress, 6);
+
 	/* Set the AC --> CGRAM Address */
-	LCD_voidSendCmnd(Copy_u8CGLocat);
-	
-	
-	/*switch(Copy_u8CGLocat)
-	{
-		case 0:
-			LCD_voidSendCmnd(0b01000000);
-		break;
-			
-		default:
-			LCD_voidSendCmnd(0b01000000 + (Copy_u8CGLocat * 8));
-		break;
-	}//end switch 
-	*/
-	
-	
-	/* Draw the wanted pattern */
+	LCD_voidSendCmnd(Local_u8CGRAMAddress);
+
+	/* Draw the pattern in CGRAM */
 	u8 i = 0;
 	while(i<=7)
 	{
@@ -307,7 +275,7 @@ void LCD_voidSendSpecialChar(u8 Copy_u8CGLocat, u8 Copy_u8LineNum, u8 Copy_u8Loc
 	
 	/* Display the pattern at DDRAM */
 	LCD_u8GoToXY(Copy_u8LineNum, Copy_u8Locat);
-	LCD_voidSendChar(Copy_u8CGLocat);
+	LCD_voidSendChar(Copy_u8CGCharNum);
 
 }//end SendSpecialChar function 
 
@@ -389,14 +357,14 @@ void LCD_voidDisplayShift(u8 Copy_u8ShiftDir)
 	{
 		case LCD_SHIFT_RIGHT:
 			
-			LCD_voidSendCmnd(0b00011100);
+			LCD_voidSendCmnd(0b00011100);	// Shift the entire display to Right
 			_delay_us(40);
 			
 		break;
 		
 		case LCD_SHIFT_LEFT:
 		
-			LCD_voidSendCmnd(0b00011000);
+			LCD_voidSendCmnd(0b00011000);	// Shift the entire display to Left
 			_delay_us(40);
 		
 		break;
