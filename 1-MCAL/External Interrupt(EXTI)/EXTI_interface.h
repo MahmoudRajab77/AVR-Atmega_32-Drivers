6abd199f41376e8d481b010ea60ca75c3f8d1773
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-04-29 20:09:52 +0300

    Update EXTI_interface.h

--- 1-MCAL/External Interrupt(EXTI)/EXTI_interface.h
+++ 1-MCAL/External Interrupt(EXTI)/EXTI_interface.h
@@ -18,21 +18,21 @@
 //---------------------------------------------
 
 /* Macros for Edge Types */
-#define EXTI_u8_FALLING_EDGE			0
+#define EXTI_u8_FALLING_EDGE				0
 #define EXTI_u8_RISING_EDGE				1
-#define EXTI_u8_ANY_LOGICAL_CHANGE		2
+#define EXTI_u8_ANY_LOGICAL_CHANGE			2
 #define EXTI_u8_LOW_LEVEL				3
 
 //-------------------------------------
 /*
 	EXTIIndex ----------> - EXTI0 
-						  - EXTI1 
-						  - EXTI2
+			      - EXTI1 
+			      - EXTI2
 						 
 	EdgeType -----------> - Rising Edge 
-						  - Falling Edge 
-						  - Low Level 
-				          - Any Logical Change
+			      - Falling Edge 
+			      - Low Level 
+			      - Any Logical Change
 */
 
 u8 EXTI_u8Enable(u8 Copy_u8EXTIIndex, u8 Copy_u8EdgeType);
